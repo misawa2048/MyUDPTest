839c9b1b5e70b9164c123e303d383ae8c3846618
misawa2048 <misawa@elix.jp>
2020-07-10 13:12:01 +0900

    available to set host ip

--- Assets/UDPTest/Scripts/UDPReceiveTest.cs
+++ Assets/UDPTest/Scripts/UDPReceiveTest.cs
@@ -10,11 +10,12 @@ namespace TmUDPTest
 {
     public class UDPReceiveTest : MonoBehaviour
     {
-        readonly string KEY_NAME = "ReceivePort";
-        static readonly int RingSize = 16;
+        static readonly string PORT_KEY_NAME = "ReceivePort";
+        static readonly int RING_SIZE = 16;
+        [SerializeField] int m_port = 7001;
         [SerializeField] InputField m_portField = null;
         [SerializeField] InputField m_outputField = null;
-        [SerializeField] int m_port = 7001;
+        [SerializeField] UDPSendTest m_sendScr = null;
         private UdpClient m_udp;
         private Thread m_thread;
         private string[] m_recvTextArr;
@@ -25,7 +26,7 @@ namespace TmUDPTest
 
         private void buffClear()
         {
-            m_recvTextArr = new string[RingSize];
+            m_recvTextArr = new string[RING_SIZE];
             m_buffPtr = 0;
             for (int i = 0; i < m_recvTextArr.Length; ++i)
             {
@@ -35,18 +36,11 @@ namespace TmUDPTest
 
         void Start()
         {
-            if (PlayerPrefs.HasKey(KEY_NAME))
+            if (!m_sendScr.isActiveAndEnabled)
             {
-                Debug.Log("HasKey"+ KEY_NAME);
-                m_port = PlayerPrefs.GetInt(KEY_NAME);
-                m_portField.text = m_port.ToString();
+                m_sendScr.InitPortSettings();
             }
-            else
-            {
-                Debug.Log("!HasKey"+ KEY_NAME);
-                int.TryParse( m_portField.text, out m_port);
-                PlayerPrefs.SetInt(KEY_NAME, m_port);
-            }
+            InitPortSettings();
             m_isReceiving = true;
             buffClear();
             m_udp = null;
@@ -73,7 +67,7 @@ namespace TmUDPTest
                 ptr--;
                 if (ptr < 0)
                 {
-                    ptr += RingSize;
+                    ptr += RING_SIZE;
                 }
             }
             m_outputField.text = outStr;
@@ -89,13 +83,35 @@ namespace TmUDPTest
             udpStop();
         }
 
-        public void OnEditChange(string _str)
+        public void OnPortEditChange(string _str)
         {
             string str = m_port.ToString();
             int.TryParse(m_portField.text, out m_port);
-            PlayerPrefs.SetInt(KEY_NAME, m_port);
+            PlayerPrefs.SetInt(PORT_KEY_NAME, m_port);
             str += "->"+m_port.ToString();
             Debug.Log("Change port:" + str);
+        }
+
+        public void OnClearOutputFieldButton()
+        {
+            buffClear();
+            m_outputField.text = "";
+        }
+
+        public void InitPortSettings()
+        {
+            if (PlayerPrefs.HasKey(PORT_KEY_NAME))
+            {
+                Debug.Log("HasKey" + PORT_KEY_NAME);
+                m_port = PlayerPrefs.GetInt(PORT_KEY_NAME);
+                m_portField.text = m_port.ToString();
+            }
+            else
+            {
+                Debug.Log("!HasKey" + PORT_KEY_NAME);
+                int.TryParse(m_portField.text, out m_port);
+                PlayerPrefs.SetInt(PORT_KEY_NAME, m_port);
+            }
         }
 
         private void udpStart()
@@ -128,7 +144,7 @@ namespace TmUDPTest
 
         private void ThreadMethod()
         {
-            Debug.Log(KEY_NAME);
+            Debug.Log(PORT_KEY_NAME);
             while (true)
             {
                 if (m_isReceiving)
@@ -137,7 +153,7 @@ namespace TmUDPTest
                     {
                         IPEndPoint remoteEP = null;
                         byte[] data = m_udp.Receive(ref remoteEP);
-                        m_buffPtr = (m_buffPtr + 1) % RingSize;
+                        m_buffPtr = (m_buffPtr + 1) % RING_SIZE;
                         m_recvTextArr[m_buffPtr] = Encoding.ASCII.GetString(data);
                         //Debug.Log("Recv:" + m_recvTextArr[m_buffPtr]);
                     }
--- Assets/UDPTest/Scripts/UDPSendTest.cs
+++ Assets/UDPTest/Scripts/UDPSendTest.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Net;
 using System.Net.Sockets;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading;
 using UnityEngine;
@@ -10,31 +11,28 @@ namespace TmUDPTest
 {
     public class UDPSendTest : MonoBehaviour
     {
-        readonly string KEY_NAME = "SendPort";
-        public string host = "localhost";
-        [SerializeField] InputField m_portField = null;
+        readonly string HOST_KEY_NAME = "SendHost";
+        readonly string PORT_KEY_NAME = "SendPort";
+        [SerializeField] string m_host = "localhost";
         [SerializeField] int m_port = 7001;
+        [SerializeField] Text m_hostText = null;
+        [SerializeField] InputField m_hostField = null;
+        [SerializeField] InputField m_portField = null;
+        [SerializeField] UDPReceiveTest m_receiveSce = null;
         private UdpClient m_udp = null;
 
         // Start is called before the first frame update
         void Start()
         {
-            if (PlayerPrefs.HasKey(KEY_NAME))
+            if (!m_receiveSce.isActiveAndEnabled)
             {
-                Debug.Log("HasKey"+ KEY_NAME);
-                m_port = PlayerPrefs.GetInt(KEY_NAME);
-                m_portField.text = m_port.ToString();
+                m_receiveSce.InitPortSettings();
             }
-            else
-            {
-                Debug.Log("!HasKey"+ KEY_NAME);
-                int.TryParse(m_portField.text, out m_port);
-                PlayerPrefs.SetInt(KEY_NAME, m_port);
-            }
+            InitPortSettings();
             if (m_udp == null)
             {
                 m_udp = new UdpClient();
-                m_udp.Connect(host, m_port);
+                m_udp.Connect(m_host, m_port);
                 Debug.Log("UDPSend start.");
                 StartCoroutine(udpSendCo());
             }
@@ -47,13 +45,51 @@ namespace TmUDPTest
 
         }
 
-        public void OnEditChange(string _str)
+        public void OnHostEditChange(string _str)
+        {
+            string str = m_host.ToString();
+            m_host = m_hostField.text;
+            PlayerPrefs.SetString(HOST_KEY_NAME, m_host);
+            str += "->" + m_host.ToString();
+            Debug.Log("Change host:" + str);
+        }
+
+        public void OnPortEditChange(string _str)
         {
             string str = m_port.ToString();
             int.TryParse(m_portField.text, out m_port);
-            PlayerPrefs.SetInt(KEY_NAME, m_port);
+            PlayerPrefs.SetInt(PORT_KEY_NAME, m_port);
             str += "->" + m_port.ToString();
             Debug.Log("Change port:" + str);
+        }
+
+        public void InitPortSettings()
+        {
+            m_hostText.text = "Host("+ getIpString() +")";
+            if (PlayerPrefs.HasKey(PORT_KEY_NAME))
+            {
+                Debug.Log("HasKey" + PORT_KEY_NAME);
+                m_port = PlayerPrefs.GetInt(PORT_KEY_NAME);
+                m_portField.text = m_port.ToString();
+            }
+            else
+            {
+                Debug.Log("!HasKey" + PORT_KEY_NAME);
+                int.TryParse(m_portField.text, out m_port);
+                PlayerPrefs.SetInt(PORT_KEY_NAME, m_port);
+            }
+
+            if (PlayerPrefs.HasKey(HOST_KEY_NAME))
+            {
+                Debug.Log("HasKey" + HOST_KEY_NAME);
+                m_host = PlayerPrefs.GetString(HOST_KEY_NAME);
+                m_hostField.text = m_host.ToString();
+            }
+            else
+            {
+                Debug.Log("!HasKey" + HOST_KEY_NAME);
+                PlayerPrefs.SetString(HOST_KEY_NAME, m_host);
+            }
         }
 
         IEnumerator udpSendCo()
@@ -80,6 +116,58 @@ namespace TmUDPTest
                 m_udp = null;
                 Debug.Log("UDP closed.");
             }
+        }
+
+        private string getIpString()
+        {
+            string retStr = GetIP(); // "127.0.0.1";
+            return retStr;
+        }
+
+        // https://stackoverflow.com/questions/51975799/how-to-get-ip-address-of-device-in-unity-2018
+        public enum ADDRESSFAM{ IPv4, IPv6 }
+        public static string GetIP(ADDRESSFAM Addfam = ADDRESSFAM.IPv4)
+        {
+            //Return null if ADDRESSFAM is Ipv6 but Os does not support it
+            if (Addfam == ADDRESSFAM.IPv6 && !Socket.OSSupportsIPv6)
+            {
+                return null;
+            }
+
+            string output = "";
+
+            foreach (NetworkInterface item in NetworkInterface.GetAllNetworkInterfaces())
+            {
+#if UNITY_EDITOR_WIN || UNITY_STANDALONE_WIN
+                NetworkInterfaceType _type1 = NetworkInterfaceType.Wireless80211;
+                NetworkInterfaceType _type2 = NetworkInterfaceType.Ethernet;
+
+                if ((item.NetworkInterfaceType == _type1 || item.NetworkInterfaceType == _type2) && item.OperationalStatus == OperationalStatus.Up)
+#endif
+                {
+                    foreach (UnicastIPAddressInformation ip in item.GetIPProperties().UnicastAddresses)
+                    {
+                        //IPv4
+                        if (Addfam == ADDRESSFAM.IPv4)
+                        {
+                            if (ip.Address.AddressFamily == AddressFamily.InterNetwork)
+                            {
+                                output = ip.Address.ToString();
+                            }
+                        }
+
+                        //IPv6
+                        else if (Addfam == ADDRESSFAM.IPv6)
+                        {
+                            if (ip.Address.AddressFamily == AddressFamily.InterNetworkV6)
+                            {
+                                output = ip.Address.ToString();
+                            }
+                        }
+                    }
+                }
+            }
+            return output;
         }
     }
 }
